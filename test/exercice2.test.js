68bd30bb5dcc3918f06268f85a848347b520a81b
Fanamby <hei.fanamby@gmail.com>
2025-10-07 11:32:08 +0300

    delete files

--- test/exercice2.test.js
+++ test/exercice2.test.js
@@ -3,10 +3,10 @@ import { findOdd } from '../code/exercice2.js';
 
 describe("findOdd", () => {
     it("should return 4 for [1,2,2,3,3,3,4,3,3,3,2,2,1]", () => {
-    expect(findOdd([1,2,2,3,3,3,4,3,3,3,2,2,1])).to.be.equal(4);
+    expect(findOdd([1,2,2,3,3,3,4,3,3,3,2,2,1])).to.deep.equal(4);
     });
 
     it ("should return 0 for [0]" , () => {
-        expect(findOdd([0])).to.be.equal(0);
+        expect(findOdd([0])).to.deep.equal(0);
     })
 })
